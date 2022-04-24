59b4cc782677eead1c4b418a28df0d8d8900bc8e
Aliaksandr <koracell@yandex.ru>
2022-04-24 03:43:55 +0300

    add profiel and error pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {buildHtmlAuthorization} from "./pages/authorization/authorization.tmpl"
 import {buildHtmlRegistration} from "./pages/registration/registration.tmpl"
 import {buildHtmlChats} from "./pages/chats/chats.tmpl"
+import {buildHtmlProfile} from "./pages/profile/profile.tmpl"
+import './pages/error/error.scss'
+import tmpl_error from './pages/error/error.hbs'
 
 const root = document.getElementById('root')
 
@@ -12,6 +15,12 @@ if(current_path == '/registration') {
     root.innerHTML = buildHtmlAuthorization();
 } else if(current_path == '/chats') {
     root.innerHTML = buildHtmlChats();
+} else if(current_path == '/profile') {
+    root.innerHTML = buildHtmlProfile();
+} else if(current_path == '/500') {
+    root.innerHTML = tmpl_error({errorStatus: '500', errorMessage: 'Мы уже фиксим'});
+} else if(current_path == '/404') {
+    root.innerHTML = tmpl_error({errorStatus: '400', errorMessage: 'Не туда попали'});
 } else {
     window.location = '/authorization'
 }
---
+++ src/pages/profile/profile.tmpl.js
@@ -0,0 +1,55 @@
+import {field} from '../../components/field/field.tmpl'
+import {button} from '../../components/button/button.tmpl'
+import tmpl from './profile.hbs'
+import './profile.scss';
+
+const fields = {
+    first_name: {
+        name: 'first_name',
+        placeholder: 'first_name',
+        klass: 'profile-form__input'
+    },
+    second_name: {
+        name: 'seasecond_namerch',
+        placeholder: 'second_name',
+        klass: 'profile-form__input'
+    },
+    display_name: {
+        name: 'display_name',
+        placeholder: 'display_name',
+        klass: 'profile-form__input'
+    },
+    login: {
+        name: 'login',
+        placeholder: 'login',
+        klass: 'profile-form__input'
+    },
+    email: {
+        name: 'email',
+        placeholder: 'email',
+        klass: 'profile-form__input'
+    },
+    phone: {
+        name: 'phone',
+        placeholder: 'phone',
+        klass: 'profile-form__input'
+    },
+    button: {
+        name: 'save',
+        klass: 'profile-form__button'
+    }
+}
+
+
+export const buildHtmlProfile = () => {
+    return tmpl({
+        firstNameField: field(fields.first_name),
+        secondNameField: field(fields.second_name),
+        displayNameField: field(fields.display_name),
+        loginField: field(fields.login),
+        emailField: field(fields.email),
+        phoneField: field(fields.phone),
+        button: button(fields.button),
+        avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png'
+    })
+}
